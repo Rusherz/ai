c51e2ff1f7e665eec894eff6c213123d65807071
Austin Medeiros <austin.medeiros@jmir.org>
2019-11-07 18:19:22 -0500

    Added multiprocessesing

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import spacy
 import random
+import multiprocessing
 
 import sqlalchemy as db
 
@@ -24,6 +25,10 @@ annotations = session.query(Annotation).limit(1000).all()
 
 TRAIN_DATA = []
 
+nlp = spacy.blank('en')  # create blank Language class
+
+optimizer = []
+
 for annotation in annotations:
     TRAIN_DATA.append((
         annotation.document, {
@@ -38,7 +43,8 @@ for annotation in annotations:
     ))
 
 def train_spacy(iterations):
-    nlp = spacy.blank('en')  # create blank Language class
+    global nlp
+
     # create the built-in pipeline components and add them to the pipeline
     # nlp.create_pipe works for built-ins that are registered with spaCy
     if 'ner' not in nlp.pipe_names:
@@ -55,22 +61,37 @@ def train_spacy(iterations):
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
     with nlp.disable_pipes(*other_pipes):  # only train NER
         optimizer = nlp.begin_training()
-        for itn in range(iterations):
-            print("Statring iteration " + str(itn))
-            random.shuffle(TRAIN_DATA)
-            losses = {}
-            for text, annotations in TRAIN_DATA:
-                nlp.update(
-                    [text],  # batch of texts
-                    [annotations],  # batch of annotations
-                    drop=0.2,  # dropout - make it harder to memorise data
-                    sgd=optimizer,  # callable to update weights
-                    losses=losses)
-            print(losses)
+
+        pool = multiprocessing.Pool(processes=4)
+        split_iterations = []
+
+        for i in range(4):
+            split_iterations.append((int(iterations / 4), optimizer))
+
+        print(split_iterations)
+        results = pool.map(update_spacy, split_iterations)
+        pool.close()
+        pool.join()
+
     return nlp
 
+def update_spacy(data):
+    optimizer = data[1]
 
-prdnlp = train_spacy(5)
+    for itn in range(data[0]):
+        print("Statring iteration " + str(itn))
+        random.shuffle(TRAIN_DATA)
+        losses = {}
+        for text, annotations in TRAIN_DATA:
+            nlp.update(
+                [text],  # batch of texts
+                [annotations],  # batch of annotations
+                drop=0.2,  # dropout - make it harder to memorise data
+                sgd=optimizer,  # callable to update weights
+                losses=losses)
+        print(losses)
+
+prdnlp = train_spacy(20)
 
 # Save our trained Model
 modelfile = input("Enter your Model Name: ")
