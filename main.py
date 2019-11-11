7f9aae627fdfc252e8e65230a18b60ecf854de64
Austin Medeiros <austin.medeiros@jmir.org>
2019-11-11 12:53:21 -0500

    Changes

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from anago.utils import load_data_and_labels
 x_train, y_train = load_data_and_labels('./data/conll2003/en/ner/train.txt')
 x_test, y_test = load_data_and_labels('./data/conll2003/en/ner/test.txt')
 
-model = anago.Sequence()
+model = anago.Sequence().load(params_file='./params.h5', preprocessor_file='./pre.h5', weights_file='./weights.h5')
 model.fit(x_train, y_train, epochs=1)
-model.save(params_file='./params.h5', preprocessor_file='pre.h5', weights_file='weights.h5')
+model.save(params_file='./params.h5', preprocessor_file='./pre.h5', weights_file='./weights.h5')
 
 model.score(x_test, y_test)
