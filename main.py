27da28fee3f9536f57dc8f19bcf66cea02c12cb5
Austin Medeiros <austin@jmir.org>
2019-11-11 13:13:49 -0500

    Changes.

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import anago
 
 from anago.utils import load_data_and_labels
 
-x_train, y_train = load_data_and_labels('./data/conll2003/en/ner/train.txt')
+x_train, y_train = load_data_and_labels('./data.txt')
 x_test, y_test = load_data_and_labels('./data/conll2003/en/ner/test.txt')
 
 model = anago.Sequence().load(params_file='./params.h5', preprocessor_file='./pre.h5', weights_file='./weights.h5')
-model.fit(x_train, y_train, epochs=1)
-model.save(params_file='./params.h5', preprocessor_file='./pre.h5', weights_file='./weights.h5')
+#model.fit(x_train, y_train, epochs=1)
+#model.save(params_file='./params.h5', preprocessor_file='./pre.h5', weights_file='./weights.h5')
 
-model.score(x_test, y_test)
+#print(model.score(x_test, y_test))
+
+import re
+
+text = re.sub('<[^<]+>', '', open('./168b2c68efa3c1d0c224ed2af8d845df.xml').read())
+
+entities = model.analyze(text)
+
+f = open('./results.json', 'w')
+f.writelines(entities)
+f.close()
+
